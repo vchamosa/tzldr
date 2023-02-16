06516b37d7d39793d399f02fa9f62e6b4c5cd8a7
vchamosa <37210240+vchamosa@users.noreply.github.com>
2023-02-16 00:24:01 +0000

    Added Tzolk'in and Haab'

--- tzolkindrier.sh
+++ tzolkindrier.sh
@@ -23,6 +23,19 @@ rem=$(($sinced0%360))
 uinal=$(($rem/20))
 kin=$(($rem%20))
 
-echo $baktun"."$katun"."$tun"."$uinal"."$kin
+#calculate Tzolk'in
+tzolkin_nms=("Imix" "Ik'" "Ak'b'al" "K'an" "Chikchan" "Kimi" "Manik'" "Lamat" "Muluk" "Ok" "Chuwen" "Eb'" "B'en" "Ix" "Men" "Kib'" "Kab'an" "Etz'nab'" "Kawak" "Ajaw")
+tzolkin_no=$((($sinced0+4)%13))
+tzolkin_nm=$(($sinced0%20))
+tzolkin_nm=${tzolkin_nms[(tzolkin_nm-1)]}
+
+#calculate Haab'
+haab_nms=("Pop" "Yax" "Wo'" "Sak'" "Sip" "Keh" "Sotz'" "Mak" "Sek" "K'ank'in" "Xul" "Muwan" "Yaxk'in" "Pax" "Mol" "K'ayab" "Ch'en" "Kumk'u" "Wayeb")
+haab_nm=$((((($sinced0-17)%365)+1)/20))
+haab_nm=${haab_nms[(haab_nm-1)]}
+haab_no=$(((($sinced0-17)%365)%20))
+
+
+echo $baktun"."$katun"."$tun"."$uinal"."$kin"  "$tzolkin_no" "$tzolkin_nm"  "$haab_no" "$haab_nm
 
 
